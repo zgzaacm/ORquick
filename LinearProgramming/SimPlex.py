7ae90735d64d890aeff25bb74f4a683825ba8ed6
Xie Yuning <41298248+xiexixixi@users.noreply.github.com>
2019-09-26 19:00:01 +0800

    注释格式

--- LinearProgramming/SimPlex.py
+++ LinearProgramming/SimPlex.py
@@ -6,87 +6,86 @@ Created on Sat Sep 21 11:54:56 2019
 """
 import numpy as np
 import copy
-from  BasicFun import Artificial,phase
+from BasicFun import Artificial, phase
+
 
 def simplex(LPP):
-    
     LP = copy.copy(LPP)
-    
-    #preprocessing
-    LP,base_index,artificial_var,ori_var = Artificial(LP)
-         
-    #判断
+
+    # preprocessing
+    LP, base_index, artificial_var, ori_var = Artificial(LP)
+
+    # 判断
     if len(artificial_var):
-        #phase 1
-    
+        # phase 1
+
         c1 = np.zeros_like(LP.c, dtype=np.float)
         c1[artificial_var] = -1
-        
-        LP,base_index,_ = phase('origin', LP, base_index, c1, artificial_var)
-        
-        #phase 2
+
+        LP, base_index, _ = phase('origin', LP, base_index, c1, artificial_var)
+
+        # phase 2
         if base_index is None:
             return None, None, None
-        
-#-----------------------------------------------------
+
+        # -----------------------------------------------------
         for i in range(len(base_index)):
             for j in range(len(artificial_var)):
-                if(base_index[i] > artificial_var[j]):
+                if (base_index[i] > artificial_var[j]):
                     base_index[i] -= 1
-                    
-#-----------------------------------------------------        
-        
+
+        # -----------------------------------------------------
+
         c2 = LP.c[ori_var]
 
-        LP.A = LP.A[:,[ori_var]]
+        LP.A = LP.A[:, [ori_var]]
         LP.A = LP.A.squeeze()
-        
+
         LP, base_index, opt_solution = phase('origin', LP, base_index, c2)
-        
+
         if opt_solution is None and LP.opt == 'min':
             return None, -np.inf, base_index
         elif opt_solution is None and LP.opt == 'max':
             return None, np.inf, base_index
-        
-        
-        opt_val = np.sum(opt_solution*c2)
-#---------------------------------------------------        
+
+        opt_val = np.sum(opt_solution * c2)
+        # ---------------------------------------------------
         count = 0
         for i in range(len(LP.scope)):
             if LP.scope[i] == -1:
                 opt_solution[i] = -opt_solution[i]
             elif LP.scope[i] == 0:
-                opt_solution[i] -= opt_solution[LP.A.shape[1]+count]
+                opt_solution[i] -= opt_solution[LP.A.shape[1] + count]
                 LP.c += 1
-                
+
         opt_solution = opt_solution[0:LP.A.shape[1]]
-#---------------------------------------------------        
-        if LP.opt=='min':
-            return opt_solution,-opt_val, base_index
+        # ---------------------------------------------------
+        if LP.opt == 'min':
+            return opt_solution, -opt_val, base_index
         else:
-            return opt_solution,opt_val, base_index
-            
+            return opt_solution, opt_val, base_index
+
     else:
         LP, base_index, opt_solution = phase('origin', LP, base_index, LP.c)
-        
+
         if opt_solution is None:
             return None, np.inf, base_index
-        
-        opt_val = np.sum(opt_solution*LP.c)
-        
-#---------------------------------------------------        
+
+        opt_val = np.sum(opt_solution * LP.c)
+
+        # ---------------------------------------------------
         count = 0
         for i in range(len(LP.scope)):
             if LP.scope[i] == -1:
                 opt_solution[i] = -opt_solution[i]
             elif LP.scope[i] == 0:
-                opt_solution[i] -= opt_solution[LP.A.shape[1]+count]
+                opt_solution[i] -= opt_solution[LP.A.shape[1] + count]
                 LP.c += 1
-                
+
         opt_solution = opt_solution[0:LP.A.shape[1]]
-#---------------------------------------------------        
-                    
-        if LP.opt=='min':
-            return opt_solution,-opt_val, base_index
+        # ---------------------------------------------------
+
+        if LP.opt == 'min':
+            return opt_solution, -opt_val, base_index
         else:
-            return opt_solution,opt_val, base_index
+            return opt_solution, opt_val, base_index
--- LinearProgramming/__init__.py
+++ LinearProgramming/__init__.py
@@ -4,4 +4,3 @@ Created on Mon Sep 23 19:01:38 2019
 
 @author: Dell
 """
-
